198d8413b15104fb1dc077f37480cf909ddcc4ed
jyotisood <jyotisoodmta@gmail.com>
2020-07-01 00:44:21 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,79 +1,74 @@
-from flask import Flask,render_template,url_for,request, jsonify
-import pandas as pd 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import SVC
-import nltk
-import re
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+import pandas as pd
+import folium
+import numpy as np
+
+def map_maker():
+    import folium
+    #url to the 
+    import datetime
+    today= datetime.datetime.today() 
+    yesterday=datetime.timedelta(days=1)
+    yesterday_str= (today-yesterday).strftime('%m-%d-%Y')
+    url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{yesterday_str}.csv'
+    
+
+    #read in data
+    corona_df = pd.read_csv(url)
+    corona_df= corona_df.loc[corona_df['Country_Region']== 'India']
+    corona_df.dropna(subset=['Lat'], inplace = True)
+    lat1 = 28.646519
+    long1 = 77.10898
+    m = folium.Map(location = [lat1, long1], zoom_start=5, tiles = 'Stamen toner')
+    folium.Circle(location = [lat1, long1], 
+                  radius = 1000, 
+                  color = 'red', 
+                  fill = True).add_to(m)
+
+    def circle_maker(x):
+        folium.Circle(location = [x[0], x[1]], 
+                  radius = float(np.max([x[2]*3, 5])), 
+                  color = 'red', 
+                  popup = f'<h5 style="backgroundcolor:black;fontcolor:white">{x[4]}</h5>\n<strong>Deaths</strong>:  {x[3]}</h5>\n<strong>Confirmed</strong>:  {x[2]}',
+                  fill = True).add_to(m)
+
+    corona_df[['Lat', 'Long_', 'Confirmed', 'Deaths','Combined_Key']].iloc[1:].apply(lambda x: circle_maker(x), axis = 1);
+    return m._repr_html_()
+
+def find_top_confirmed(n = 15):
+    '''
+    This function accepts a date and returns
+    DataFrames with confirmed, death, recovered,
+    and active information on top n countries.
+    '''
+    import datetime
+    today= datetime.datetime.today() 
+    yesterday=datetime.timedelta(days=1)
+    yesterday_str= (today-yesterday).strftime('%m-%d-%Y')
+    url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{yesterday_str}.csv'
+    corona_df = pd.read_csv(url)
+
+        
+     
+    corona_df= corona_df.loc[corona_df['Country_Region']== 'India']
+    corona_df.dropna(subset=['Lat'], inplace = True)
+    by_state = corona_df.groupby('Province_State').sum()[['Confirmed', 'Deaths', 'Recovered', 'Active']]
+    cdf = by_state.nlargest(n, 'Confirmed')[['Confirmed']]
+    return cdf
+   
+
+dfs = find_top_confirmed()
+pairs = [(a, b) for a,b in zip(dfs.index, dfs['Confirmed'])]
+cdf = dfs.to_html()    
+
+cmap = map_maker()
+from flask import Flask, render_template
 
 app = Flask(__name__)
-global Classifier
-global Vectorizer
-stop_words = nltk.corpus.stopwords.words('english')
-porter = nltk.PorterStemmer()
-
-
-def preprocess_data(data):
-    data = str(data)
-    cleaned = re.sub(r'\b[\w\-.]+?@\w+?\.\w{2,4}\b', 'emailaddr', data)
-    cleaned = re.sub(r'(http[s]?\S+)|(\w+\.[A-Za-z]{2,4}\S*)', 'httpaddr',cleaned)
-    cleaned = re.sub(r'£|\$', 'moneysymb', cleaned)
-    cleaned = re.sub(r'\b(\+\d{1,2}\s)?\d?[\-(.]?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b','phonenumbr', cleaned)
-    cleaned = re.sub(r'\d+(\.\d+)?', 'numbr', cleaned)
-    cleaned = re.sub(r'[^\w\d\s]', ' ', cleaned)
-    cleaned = re.sub(r'\s+', ' ', cleaned)
-    cleaned = re.sub(r'^\s+|\s+?$', '', cleaned.lower())
-    return ' '.join(
-        porter.stem(term) 
-        for term in cleaned.split()
-        if term not in set(stop_words)
-    )
-
-# load data
-data = pd.read_csv('data/sms-spam-collection-dataset/spam.csv', encoding='latin-1')
-train_data = data[:4400] # 4400 items
-test_data = data[4400:] # 1172 items
-preprocess_data(train_data)
-preprocess_data(test_data)
-
-
-
-# train model
-Classifier = OneVsRestClassifier(SVC(kernel='linear', probability=True))
-Vectorizer = TfidfVectorizer()
-vectorize_text = Vectorizer.fit_transform(train_data.v2)
-Classifier.fit(vectorize_text, train_data.v1)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('df.html', frame = cdf, vmap = cmap, pairs= pairs)
 
-@app.route('/predict',methods=['POST'])
-def predict():
-	error = ''
-	predict = ''
-	if request.method == 'POST':
-		message = request.form['message']
-        
-        
-        
-		try:
-			if len(message) > 0:
-				vectorize_message = Vectorizer.transform([preprocess_data(message)])
-				predict = Classifier.predict(vectorize_message)[0]
-				
-			else:
-				predict = 2
-		except BaseException as inst:
-			error = str(type(inst).__name__) + ' ' + str(inst)
-	if (predict == 'ham'):
-		predict = 1
-	elif (predict == 'spam'):
-		predict = 0
-	return render_template('result.html', prediction = predict)
 
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=False)
+    app.run(debug = True,  use_reloader=False)
